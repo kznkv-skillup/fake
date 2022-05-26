4a7e8f3e1ec315cd5e239b0fe4c9f9c96d82400b
Yaroslav <kznkv.test@gmail.com>
2022-05-26 20:27:31 +0300

    add basic structure and absolute imports

---
+++ src/container/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Header } from 'container/Header/Header'
+import { Footer } from 'container/Footer/Footer'
+import { Main } from 'container/Main/Main'
+
+export const App = () => {
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    )
+}
