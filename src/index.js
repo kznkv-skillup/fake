21a237c7e614686a7b2a51bd951cc1b15e391d9a
Yaroslav <kznkv.test@gmail.com>
2022-05-26 19:45:15 +0300

    add prettier formatter

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const Header = () => <h1>Hello App.js</h1>
+
+function AppList() {
+    return (
+        <ul>
+            <li>list item 1</li>
+            <li>list item 2</li>
+            <li>list item 3</li>
+        </ul>
+    )
+}
+
+function App() {
+    return (
+        <>
+            <Header />
+            <AppList />
+        </>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
