bf61a1f2af0d8d860055add2a4e5c0a8d5e67004
Yaroslav <kznkv.test@gmail.com>
2022-05-31 20:22:45 +0300

    add ProductListItem

---
+++ src/components/Products/ProductListItem.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Card, CardActions, CardContent, Button } from '@mui/material'
+import './ProductListItem.scss'
+
+export const ProductListItem = () => {
+    return (
+        <Card>
+            <CardContent>
+                <h4>iPhone XS</h4>
+                <p>This is iPhone XS</p>
+                <div>Type: phone</div>
+                <div>Capacity: 64Gb</div>
+                <div>500 $</div>
+            </CardContent>
+            <CardActions className="add-to-cart-block">
+                <Button variant="contained">Add to cart</Button>
+            </CardActions>
+        </Card>
+    )
+}
